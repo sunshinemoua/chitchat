c4cb8c3af875c4823186e2a6d064963ccb11acdf
sunshinemoua <sunshinemoua21@gmail.com>
2024-10-30 13:36:03 -0700

    create sign out btn; pulled name and img from google session

--- src/app/components/UserButton.tsx
+++ src/app/components/UserButton.tsx
@@ -12,7 +12,7 @@ import {
 import UserAvatar from "./UserAvatar";
 import { Session } from "next-auth";
 import { Button } from "@/components/ui/button";
-import { signIn } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 
 const UserButton = ({ session }: { session: Session | null }) => {
   // If not signed in, render button and redirect to Google login page
@@ -24,19 +24,26 @@ const UserButton = ({ session }: { session: Session | null }) => {
     );
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <UserAvatar name="Sunshine M" image="https://github.com/shadcn.png" />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    session && (
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <UserAvatar
+            name={session.user?.name || "Username"}
+            image={session.user?.image || "https://github.com/shadcn.png"}
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>
+            {session.user?.name || "My Account"}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem onClick={() => signOut()}>
+            Sign Out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
   );
 };
 
