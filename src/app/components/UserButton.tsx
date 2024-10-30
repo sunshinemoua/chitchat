8f66a0105e52b04d0c27995728829ed074896fe0
sunshinemoua <sunshinemoua21@gmail.com>
2024-10-30 13:25:00 -0700

    added sign in button

--- src/app/components/UserButton.tsx
+++ src/app/components/UserButton.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
   DropdownMenu,
@@ -8,9 +10,18 @@ import {
   DropdownMenuTrigger,
 } from "../../components/ui/dropdown-menu";
 import UserAvatar from "./UserAvatar";
+import { Session } from "next-auth";
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
 
-const UserButton = () => {
-  // ... Session ...
+const UserButton = ({ session }: { session: Session | null }) => {
+  // If not signed in, render button and redirect to Google login page
+  if (!session)
+    return (
+      <Button variant={"outline"} onClick={() => signIn()}>
+        Sign In
+      </Button>
+    );
 
   return (
     <DropdownMenu>
