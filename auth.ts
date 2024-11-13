04299705a03b8e21482bb4806733b37ee98e4b30
sunshinemoua <sunshinemoua21@gmail.com>
2024-11-12 21:03:10 -0800

    updated checkout btn; pulled session data; added session strategy jwt

--- auth.ts
+++ auth.ts
@@ -8,4 +8,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 } satisfies NextAuthOptions;
--- src/app/components/CheckoutButton.tsx
+++ src/app/components/CheckoutButton.tsx
@@ -1,7 +1,23 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useSession } from "next-auth/react";
 import React from "react";
 
 const CheckoutButton = () => {
-  return <div>CheckoutButton</div>;
+  const { data: session } = useSession();
+
+  const createCheckoutSession = async () => {
+    if (!session) return;
+  };
+
+  return (
+    <Button
+      onClick={() => createCheckoutSession()}
+      className="mt-8 bg-indigo-600 text-white hover:bg-indigo-500"
+    >
+      Sign Up
+    </Button>
+  );
 };
 
 export default CheckoutButton;
