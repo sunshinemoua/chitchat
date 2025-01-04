85c78cd7de5b8d3b510de2108f411cc23a73ddf5
sunshinemoua <sunshinemoua21@gmail.com>
2025-01-04 14:19:08 -0800

    added price api id from stripe

--- src/app/components/CheckoutButton.tsx
+++ src/app/components/CheckoutButton.tsx
@@ -1,13 +1,28 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { addDoc, collection } from "firebase/firestore";
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { useState } from "react";
+import { db } from "../../../firebase";
 
 const CheckoutButton = () => {
   const { data: session } = useSession();
+  const [loading, setLoading] = useState(false);
 
   const createCheckoutSession = async () => {
-    if (!session) return;
+    if (!session?.user.id) return;
+
+    // Push a document into firestore db
+    setLoading(true);
+
+    const docRef = await addDoc(
+      collection(db, "customers", session.user.id, "checkout_sessions"),
+      {
+        price: "price_1QdfUqFjtkHrjCdS7xmNolFW",
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      }
+    );
   };
 
   return (
