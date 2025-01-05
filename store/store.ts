f6fc0c3ad3580de6b744cbb8e07c38fdc37637a5
sunshinemoua <sunshinemoua21@gmail.com>
2025-01-05 15:26:44 -0800

    added zustand store

---
+++ store/store.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+import { Subscription } from "../types/Subscription";
+
+interface SubscriptionState {
+  subscription: Subscription | null | undefined;
+  setSubscription: (subscription: Subscription | null) => void;
+}
+
+// Zustand store for managing subscription state with setter func
+export const userSubscriptionStore = create<SubscriptionState>((set) => ({
+  subscription: null,
+  setSubscription: (subscription: Subscription | null) => set({ subscription }),
+}));
