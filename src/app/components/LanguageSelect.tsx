b0e91804869d4719e86f231c3d5738d65845b447
sunshinemoua <sunshinemoua21@gmail.com>
2025-01-08 16:46:12 -0800

    added search bar inside languages tab

--- src/app/components/LanguageSelect.tsx
+++ src/app/components/LanguageSelect.tsx
@@ -1,70 +1,147 @@
 "use client";
-import React from "react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { useLanguageStore, userSubscriptionStore } from "../../../store/store";
 import { usePathname } from "next/navigation";
-import {
-  Select,
-  SelectContent,
-  SelectTrigger,
-  SelectValue,
-  SelectItem,
-} from "@/components/ui/select";
-import {
-  LanguagesSupported,
-  LanguagesSupportedMap,
-} from "../../../types/Languages";
-import LoadingSpinner from "./LoadingSpinner";
-import Link from "next/link";
+import { LanguagesSupportedMap } from "../../../types/Languages";
+import { useState } from "react";
 
-const LanguageSelect = () => {
+const languageFormSchema = z.object({
+  languageQuery: z.string({
+    required_error: "Please select a language.",
+  }),
+});
+
+const SearchLanguage = () => {
   const { language, setLanguage, getLanguages, getUnsupportedLanguages } =
     useLanguageStore.getState();
-
   const subscription = userSubscriptionStore((state) => state.subscription);
+  const pathName = usePathname();
+  const [isPopoverOpen, setIsPopOverOpen] = useState(false);
+
   const isPro =
     subscription?.role == "pro" && subscription?.status === "active";
 
-  const pathName = usePathname();
   const isChatPage = pathName.includes("/chat");
+
+  // Define form
+  const form = useForm<z.infer<typeof languageFormSchema>>({
+    resolver: zodResolver(languageFormSchema),
+  });
+
+  const onSubmit = () => {
+    setLanguage(language);
+  };
 
   return (
     isChatPage && (
-      <div>
-        <Select
-          onValueChange={(value: LanguagesSupported) => setLanguage(value)}
-        >
-          <SelectTrigger className="w-[150px] text-black dark:text-white">
-            <SelectValue placeholder={LanguagesSupportedMap[language]} />
-          </SelectTrigger>
-
-          <SelectContent>
-            {subscription === undefined ? (
-              <LoadingSpinner />
-            ) : (
-              <>
-                {getLanguages(isPro).map((language) => (
-                  <SelectItem key={language} value={language}>
-                    {LanguagesSupportedMap[language]}
-                  </SelectItem>
-                ))}
-                {getUnsupportedLanguages(isPro).map((language) => (
-                  <Link href={"/register"} key={language} prefetch={false}>
-                    <SelectItem
-                      value={language}
-                      disabled
-                      className="bg-gray-300/50 text-gray-500 dark:text-white py-2 my-1"
-                    >
-                      {LanguagesSupportedMap[language]} (PRO)
-                    </SelectItem>
-                  </Link>
-                ))}
-              </>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          {/* Input for language search */}
+          <FormField
+            control={form.control}
+            name="languageQuery"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                {/* Component for dropdown */}
+                <Popover open={isPopoverOpen} onOpenChange={setIsPopOverOpen}>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        className={cn(
+                          "w-[200px] justify-between",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {/* Render selected or default language */}
+                        {field.value || LanguagesSupportedMap[language]}
+                        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-[200px] p-0">
+                    {/* Dropdown list of languages */}
+                    <Command>
+                      <CommandInput placeholder="Search language..." />
+                      <CommandList>
+                        <CommandEmpty>No language found.</CommandEmpty>
+                        <CommandGroup>
+                          {getLanguages(isPro).map((language) => (
+                            // If selected, update form value, state and close dropdown
+                            <CommandItem
+                              value={LanguagesSupportedMap[language]}
+                              key={language}
+                              onSelect={() => {
+                                form.setValue(
+                                  "languageQuery",
+                                  LanguagesSupportedMap[language]
+                                );
+                                setLanguage(language);
+                                setIsPopOverOpen(false);
+                              }}
+                            >
+                              {LanguagesSupportedMap[language]}
+                              <Check
+                                className={cn(
+                                  "ml-auto",
+                                  LanguagesSupportedMap[language] ===
+                                    field.value
+                                    ? "opacity-100"
+                                    : "opacity-0"
+                                )}
+                              />
+                            </CommandItem>
+                          ))}
+                          {/* If not pro, render list of languages as disabled with tag */}
+                          {getUnsupportedLanguages(isPro).map((language) => (
+                            <CommandItem
+                              value={LanguagesSupportedMap[language]}
+                              key={language}
+                              disabled
+                            >
+                              {LanguagesSupportedMap[language]} (PRO)
+                            </CommandItem>
+                          ))}
+                        </CommandGroup>
+                      </CommandList>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+                <FormMessage />
+              </FormItem>
             )}
-          </SelectContent>
-        </Select>
-      </div>
+          />
+        </form>
+      </Form>
     )
   );
 };
 
-export default LanguageSelect;
+export default SearchLanguage;
