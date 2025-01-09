c7206ead33912d61183ed546bfd72973cb680573
sunshinemoua <sunshinemoua21@gmail.com>
2025-01-08 22:39:56 -0800

    debug flag

--- src/app/components/CheckoutButton.tsx
+++ src/app/components/CheckoutButton.tsx
@@ -11,7 +11,6 @@ const CheckoutButton = () => {
   const { data: session } = useSession();
   const [loading, setLoading] = useState(false);
   const subscription = userSubscriptionStore((state) => state.subscription);
-
   const isSubscribed =
     subscription?.status === "active" && subscription?.role === "pro";
 
@@ -49,13 +48,14 @@ const CheckoutButton = () => {
       }
     });
   };
-
   return (
     <div className="flex flex-col space-y-2">
       <div className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
         {isSubscribed ? (
           <ManageAccountButton />
-        ) : subscription == undefined || loading ? (
+        ) : (session && subscription !== null) ||
+          (session && subscription === undefined) ||
+          loading ? (
           <LoadingSpinner />
         ) : (
           <button onClick={() => createCheckoutSession()}>Sign Up</button>
